61e300dce08fb37463c0a96e56f4768cd902d431
Ayushi Maheshwari <maheshwariayushi846@gmail.com>
2026-03-22 13:16:08 +0530

    fix: use dynamic PORT for Render

--- app.js
+++ app.js
@@ -109,6 +109,10 @@ app.use((err,req,res,next)=>{
     res.status(statusCode).render("error.ejs",{err});
    // res.status(statusCode).send(message);
 });
-app.listen(8080,()=>{
-    console.log('Server is running on port 8080');
+//app.listen(8080,()=>{
+   // console.log('Server is running on port 8080');
+//});
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
